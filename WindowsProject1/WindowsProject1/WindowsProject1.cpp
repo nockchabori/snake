b0a0c8b6ff1caf5afe85becaa96b0c384f24586a
nockchabori <yjgpark@gmail.com>
2026-09-10 18:26:44 +0900

    Add files via upload

--- WindowsProject1/WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -173,7 +173,6 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow) {
     GdiplusStartupInput gdiplusStartupInput;
     ULONG_PTR gdiplusToken = 0;
     GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
-
     gImg = Image::FromFile(L"head.png");
     apple = Image::FromFile(L"apple.png");
     body = Image::FromFile(L"body.png");
@@ -197,7 +196,7 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow) {
     kClass, L"snake",
     WS_OVERLAPPEDWINDOW,
     CW_USEDEFAULT, CW_USEDEFAULT,
-    WIN_W, WIN_H,
+    1015, 980,
     nullptr, nullptr, hInst, nullptr);
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
